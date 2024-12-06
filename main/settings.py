340efd73a01a5a8ce6ca8cb553173bffd5b8dbc7
Saymios <daniojenezis@gmail.com>
2024-12-06 13:01:01 +0200

    add requirements.txt, migrate databes sqlite3 -> Postgresql and filtration tables working 50%

--- main/settings.py
+++ main/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Coffee_times',
+        'USER': 'coffee_user',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Или адрес сервера
+        'PORT': '5432',       # Стандартный порт PostgreSQL
     }
 }
 
--- orders/views.py
+++ orders/views.py
@@ -39,52 +39,7 @@ def get_available_tables(request):
             reservation_time = data.get('reservation_time')
             booking_duration = data.get('booking_duration')
 
-            # # Конвертируем строковые данные в datetime
-            # start_time_str = f"{reservation_date} {reservation_time}"
-            # start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
-
-            # # Обработка длительности бронирования
-            # try:
-            #     duration_parts = booking_duration.split(":")
-            #     hours = int(duration_parts[0])
-            #     minutes = int(duration_parts[1])
-            #     duration = timedelta(hours=hours, minutes=minutes)
-            # except (IndexError, ValueError):
-            #     return JsonResponse({'error': 'Invalid booking_duration format. Expected "HH:MM".'}, status=400)
-
-            # end_time = start_time + duration
-
-            # # 1. Рассчитываем start_datetime и end_datetime для всех бронирований с учётом даты и времени
-            # reservations_with_end_time = Reservation.objects.annotate(
-            #     start_datetime=ExpressionWrapper(
-            #         F('reservation_date') + F('reservation_time'),
-            #         output_field=DateTimeField()
-            #     ),
-            #     end_datetime=ExpressionWrapper(
-            #         F('reservation_date') + F('reservation_time') + F('booking_duration'),
-            #         output_field=DateTimeField()
-            #     )
-            # )
-
-            # # 2. Фильтруем бронирования с пересечением времени
-            # overlapping_reservations = reservations_with_end_time.filter(
-            #     table__coffeehouse_id=coffeehouse_id,  # Учитываем кофейню
-            #     reservation_date=reservation_date,
-            #     # Бронирование должно пересекаться с новым, то есть:
-            #     #  - начнется до окончания нового бронирования
-            #     #  - или закончится после начала нового бронирования
-            #     start_datetime__lt=end_time,        # Бронирование начинается до окончания нового
-            #     end_datetime__gt=start_time         # Бронирование заканчивается после начала нового
-            # )
-
-            # # 3. Находим ID занятых столиков
-            # reserved_tables = overlapping_reservations.values_list('table_id', flat=True)
-
-            # # 4. Получаем доступные столики
-            # available_tables = Table.objects.filter(coffeehouse_id=coffeehouse_id).exclude(id__in=reserved_tables)
-
             reservations_today = Reservation.objects.filter(coffeehouse_id=coffeehouse_id ,reservation_date=reservation_date)
-            print(reservations_today)
             for item in reservations_today:
                 print(item.table)
 
@@ -99,14 +54,15 @@ def get_available_tables(request):
                 print(reservation.end_time)
 
              # Фильтруем пересекающиеся брони
-            overlapping_reservations = annotations_reservation.filter(end_time__lt=reservation_time)
-            print(reservation_time)
-
-            print(overlapping_reservations)
+            overlapping_reservations = annotations_reservation.filter(end_time__gt=reservation_time).values_list('table_id', flat=True)
+            # # Преобразуем в объект времени
+            # formatted_time = datetime.strptime(reservation_time, "%H:%M").time()
+            # TODO Добавить также позже фильтрацию по пересечению времени окончания резервациии что выбрал пользователь с существующей резервацией другого пользователя,
+            #  если есть резервация которая начинается раньше чем заканчивается резервация которую выбрал пользователей также исключаем тот столик
 
 
             # Формируем данные для ответа
-            tables_data = [{"id": table.id, 'name': table.table_number} for table in Table.objects.all()]
+            tables_data = [{"id": table.id, 'name': table.table_number} for table in Table.objects.filter(coffeehouse_id=coffeehouse_id).exclude(id__in=overlapping_reservations)]
             return JsonResponse({'tables': tables_data})
 
         except json.JSONDecodeError:
